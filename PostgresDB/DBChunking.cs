5bb9a09eab05462552f3977459d6b9054d54a82e
misssignal <misssignal01@gmail.com>
2024-11-10 13:41:59 -0500

    Create DBChunking.cs

---
+++ PostgresDB/DBChunking.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostgresDB
+{
+    public class DBChunking
+    {
+        public async void ChunkDB()
+        {
+            int pageSize = 1000;
+            int pageIndex = 0;
+            List<Food> foodItems = new List<Food>();
+
+            while (true)
+            {
+                var chunk = await dbContext.sample_table_food
+                    .OrderBy(f => f.Id)
+                    .Skip(pageIndex * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                if (!chunk.Any())
+                    break;
+
+                foodItems.AddRange(chunk);
+                pageIndex++;
+            }
+
+        }
+
+    }
+}
